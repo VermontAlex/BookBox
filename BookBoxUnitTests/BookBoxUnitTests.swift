595b3d0d6cafe62374b9f95dd18f34cb8169b589
Oleksandr Oliinyk <oleksandr.oliinyk@oOliinyk-M1pro.local>
2024-08-09 12:18:43 +0300

    BB: Remove redundant files

---
+++ BookBoxUnitTests/BookBoxUnitTests.swift
@@ -0,0 +1,13 @@
+//
+//  BookBoxUnitTests.swift
+//  BookBoxTests
+//
+//  Created by Oleksandr Oliinyk on 07.08.2024.
+//
+
+import XCTest
+@testable import BookBox
+
+final class BookBoxUnitTests: XCTestCase {
+//  MARK: To-DO Cover project with unit tests when more business logic will be implemented
+}
